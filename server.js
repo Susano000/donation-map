11d79f38127339e2a2a5e448cafd31bfd9786b14
Susano000 <22102max@gmail.com>
2026-04-16 20:10:02 +0300

    Исправления для Vercel

--- server.js
+++ server.js
@@ -280,6 +280,7 @@ app.get('/api/conquests/recent', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`🌍 Сервер запущен на порту ${PORT}`);
 });
